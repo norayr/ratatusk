95030c87ac53c2cbc56a7c4e632faa3826f590ef
Norayr Chilingarian <norayr@arnet.am>
2025-02-12 03:11:20 +0400

    trying udbserver, but in vain.

--- unit1.pas
+++ unit1.pas
@@ -6,24 +6,10 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  IdTCPClient, IdTCPServer, IdContext, IdUDPClient, IdGlobal, IdSocketHandle,
-  IdIPMCastServer, // for multicast server
+  IdTCPClient, IdTCPServer, IdContext, IdUDPServer, IdGlobal, IdSocketHandle,
   xep0174;
 
 type
-  { TMulticastReceiverThread }
-  TMulticastReceiverThread = class(TThread)
-  private
-    FServer: TIdIPMCastServer;
-    FReceivedBuffer: TIdBytes;
-
-    procedure DoProcessBuffer;
-  protected
-    procedure Execute; override;
-  public
-    constructor Create(AServer: TIdIPMCastServer);
-  end;
-
   { TForm1 }
   TForm1 = class(TForm)
     BtnSend: TButton;
@@ -31,8 +17,7 @@ type
     ListBoxContacts: TListBox;
     IdTCPClient1: TIdTCPClient;
     IdTCPServer1: TIdTCPServer;
-    IdUDPClient1: TIdUDPClient;
-    IdIPMCastServer1: TIdIPMCastServer;
+    IdUDPServer1: TIdUDPServer;
     MemoSend: TMemo;
     MemoReceived: TMemo;
     procedure BtnRefreshContactsClick(Sender: TObject);
@@ -41,12 +26,11 @@ type
     procedure FormCreate(Sender: TObject);
     procedure OnClientExecute(AContext: TIdContext);
     procedure DiscoverMDNSClients;
-    procedure OnMDNSResponse(AThread: TObject; const AData: TIdBytes;
+    procedure OnUDPRead(AThread: TIdUDPListenerThread; const AData: TIdBytes;
       ABinding: TIdSocketHandle);
     procedure UpdateListBox;
   private
     FLastReceivedMessage: string;
-    FReceiverThread: TMulticastReceiverThread;
     FLastPeerIP: string;
     procedure DisplayReceivedMessage;
   public
@@ -60,62 +44,9 @@ implementation
 
 {$R *.lfm}
 
-{===========================
-  TMulticastReceiverThread
-===========================}
-
-constructor TMulticastReceiverThread.Create(AServer: TIdIPMCastServer);
-begin
-  inherited Create(False);  // start immediately
-  FreeOnTerminate := True;
-  FServer := AServer;
-end;
-
-procedure TMulticastReceiverThread.DoProcessBuffer;
-begin
-  // Call the main form's MDNS response handler.
-  // (Passing nil for AThread.)
-  if Assigned(Form1) then
-    Form1.OnMDNSResponse(nil, FReceivedBuffer, FServer.Binding);
-end;
-
-procedure TMulticastReceiverThread.Execute;
-var
-  LBuffer: TIdBytes;
-  LBytesRead: Integer;
-  LPeerIP: string;
-  LPeerPort: Word;
-  LIPVersion: TIdIPVersion;
-begin
-  while not Terminated do
-  begin
-    // Allocate a buffer for incoming data.
-    SetLength(LBuffer, 4096);
-    // Initialize variables for the sender info.
-    LPeerIP := '';
-    LPeerPort := 0;
-    LIPVersion := Id_IPv4;  // initialize; RecvFrom will update this if needed.
-    // Call RecvFrom with the proper parameters.
-    LBytesRead := FServer.Binding.RecvFrom(LBuffer, LPeerIP, LPeerPort, LIPVersion);
-    if LBytesRead > 0 then
-    begin
-      SetLength(LBuffer, LBytesRead);
-      // Store the sender's IP address in a form field.
-      Form1.FLastPeerIP := LPeerIP;
-      // Remove the following line because PeerIP is read-only:
-      // FServer.Binding.PeerIP := LPeerIP;
-      FReceivedBuffer := LBuffer;
-      Synchronize(@DoProcessBuffer);
-    end;
-    Sleep(10); // Small delay to avoid a tight loop.
-  end;
-end;
-
-
-{===========================
+{---------------------------
   Helper Function: DecodeDomainName
-===========================}
-
+---------------------------}
 function DecodeDomainName(const AData: TIdBytes; var Offset: Integer): string;
 var
   LabelLen: Byte;
@@ -145,7 +76,7 @@ begin
     end
     else
     begin
-      Inc(Offset); // Skip the length byte.
+      Inc(Offset); // Skip length byte.
       if Offset + LabelLen > Length(AData) then Break; // Safety check.
       SetString(s, PAnsiChar(@AData[Offset]), LabelLen);
       Inc(Offset, LabelLen);
@@ -159,12 +90,15 @@ begin
   Result := resultStr;
 end;
 
-{===========================
+{---------------------------
   TForm1 Methods
-===========================}
+---------------------------}
 
 procedure TForm1.FormCreate(Sender: TObject);
+var
+  b: TIdSocketHandle;
 begin
+  // Create the roster and add a default contact.
   Roster := TRoster.Create;
   Roster.AddContact('noch', 'debet.local', '192.168.1.43', 5298);
 
@@ -172,14 +106,18 @@ begin
   IdTCPServer1.OnExecute := @OnClientExecute;
   StartListening(IdTCPServer1, 5298);
 
-  // Configure the IPMCast server for mDNS responses.
-  // Use BoundPort to specify the port to bind.
-  IdIPMCastServer1.BoundPort := 5353;            // mDNS standard port.
-  IdIPMCastServer1.MulticastGroup := '224.0.0.251';// Join the mDNS multicast group.
-  IdIPMCastServer1.Active := True;
+ // Configure the UDP server for mDNS responses.
+IdUDPServer1.Active := False;
+IdUDPServer1.Bindings.Clear;
+IdUDPServer1.ReuseSocket := rsTrue;   // <--- Add this line here
+b := IdUDPServer1.Bindings.Add;
+b.IP := '0.0.0.0';  // Listen on all interfaces.
+b.Port := 5353;
+// Join the multicast group using the binding's AddMulticastMembership method.
+b.AddMulticastMembership('224.0.0.251');
+IdUDPServer1.OnUDPRead := @OnUDPRead;
+IdUDPServer1.Active := True;
 
-  // Start our multicast receiver thread.
-  FReceiverThread := TMulticastReceiverThread.Create(IdIPMCastServer1);
 end;
 
 procedure TForm1.DiscoverMDNSClients;
@@ -188,41 +126,34 @@ var
   DestIP: string;
   Index: Integer;
 begin
-  DestIP := '224.0.0.251';  // mDNS multicast group.
-  SetLength(Query, 38);     // Allocate 38 bytes for the query.
+  DestIP := '224.0.0.251';  // multicast group address.
+  SetLength(Query, 38);     // allocate 38 bytes for the query
   // DNS Header (12 bytes)
-  Query[0] := 0; Query[1] := 0;   // Transaction ID = 0.
-  Query[2] := 0; Query[3] := 0;   // Flags = 0.
-  Query[4] := 0; Query[5] := 1;   // Questions = 1.
-  Query[6] := 0; Query[7] := 0;   // Answer RRs = 0.
-  Query[8] := 0; Query[9] := 0;   // Authority RRs = 0.
-  Query[10] := 0; Query[11] := 0; // Additional RRs = 0.
-  // Query Name "_presence._tcp.local"
+  Query[0] := 0; Query[1] := 0;     // Transaction ID = 0.
+  Query[2] := 0; Query[3] := 0;     // Flags = 0.
+  Query[4] := 0; Query[5] := 1;     // Questions = 1.
+  Query[6] := 0; Query[7] := 0;     // Answer RRs = 0.
+  Query[8] := 0; Query[9] := 0;     // Authority RRs = 0.
+  Query[10] := 0; Query[11] := 0;   // Additional RRs = 0.
+  // Query Name: "_presence._tcp.local"
   Index := 12;
-  Query[Index] := 9; Inc(Index);  // Length of "_presence"
+  Query[Index] := 9; Inc(Index);
   Move('_presence', Query[Index], 9); Inc(Index, 9);
-  Query[Index] := 4; Inc(Index);  // Length of "_tcp"
+  Query[Index] := 4; Inc(Index);
   Move('_tcp', Query[Index], 4); Inc(Index, 4);
-  Query[Index] := 5; Inc(Index);  // Length of "local"
+  Query[Index] := 5; Inc(Index);
   Move('local', Query[Index], 5); Inc(Index, 5);
-  Query[Index] := 0; Inc(Index);  // Null terminator.
+  Query[Index] := 0; Inc(Index);  // null terminator.
   // Query Type (PTR = 0x000C)
   Query[Index] := 0; Query[Index+1] := 12; Inc(Index, 2);
-  // Query Class (IN = 0x0001) with unicast response bit (0x8001)
-  //Query[Index] := $80; Query[Index+1] := 1; Inc(Index, 2);
-  // the above replaced with
-  // Query Class (IN = 0x0001) without the unicast response bit,
-  // so that responders send multicast replies.
+  // Query Class (IN = 0x0001) without unicast response bit.
   Query[Index] := 0; Query[Index+1] := 1; Inc(Index, 2);
 
-
-  // Send the query using the UDP client.
-  IdUDPClient1.Host := DestIP;
-  IdUDPClient1.Port := 5353;
-  IdUDPClient1.SendBuffer(Query);
+  // Send the query using the UDP server (so that the source port is 5353).
+  IdUDPServer1.SendBuffer(DestIP, 5353, Query);
 end;
 
-procedure TForm1.OnMDNSResponse(AThread: TObject; const AData: TIdBytes;
+procedure TForm1.OnUDPRead(AThread: TIdUDPListenerThread; const AData: TIdBytes;
   ABinding: TIdSocketHandle);
 var
   curPos: Integer;
@@ -246,7 +177,7 @@ begin
   ANCount := (AData[curPos] shl 8) or AData[curPos + 1];
   Inc(curPos, 2);
   Inc(curPos, 4);  // Skip NSCount and ARCount.
-  // Skip Question Section.
+  // Skip the Question Section.
   for i := 1 to QDCount do
   begin
     dummy := DecodeDomainName(AData, curPos);
@@ -258,14 +189,14 @@ begin
   begin
     rrName := DecodeDomainName(AData, curPos);
     if curPos + 10 > Length(AData) then Break;
-    rrType := (AData[curPos] shl 8) or AData[curPos + 1];
+    rrType := (AData[curPos] shl 8) or AData[curPos+1];
     Inc(curPos, 2);
-    rrClass := (AData[curPos] shl 8) or AData[curPos + 1];
+    rrClass := (AData[curPos] shl 8) or AData[curPos+1];
     Inc(curPos, 2);
-    rrTTL := (AData[curPos] shl 24) or (AData[curPos + 1] shl 16) or
-             (AData[curPos + 2] shl 8) or AData[curPos + 3];
+    rrTTL := (AData[curPos] shl 24) or (AData[curPos+1] shl 16) or
+             (AData[curPos+2] shl 8) or AData[curPos+3];
     Inc(curPos, 4);
-    rrDataLength := (AData[curPos] shl 8) or AData[curPos + 1];
+    rrDataLength := (AData[curPos] shl 8) or AData[curPos+1];
     Inc(curPos, 2);
     // Look for a PTR record (type 12).
     if rrType = 12 then
@@ -283,11 +214,12 @@ begin
       PeerName := Copy(PeerFullName, 1, i - 1)
     else
       PeerName := PeerFullName;
-  end else
+  end
+  else
     PeerName := '';
-  if (PeerName <> '') and (FLastPeerIP <> '') then
+  if (PeerName <> '') and (ABinding.PeerIP <> '') then
   begin
-    MemoReceived.Lines.Add('Extracted Peer: ' + PeerName + ' (' + FLastPeerIP + ')');
+    MemoReceived.Lines.Add('Extracted Peer: ' + PeerName + ' (' + ABinding.PeerIP + ')');
     FLastReceivedMessage := PeerName;
     ExistsInRoster := False;
     for i := 0 to Length(Roster.Contacts) - 1 do
@@ -300,18 +232,21 @@ begin
     end;
     if not ExistsInRoster then
     begin
-      Roster.AddContact(PeerName, PeerName + '.local', FLastPeerIP, 5298);
+      Roster.AddContact(PeerName, PeerName + '.local', ABinding.PeerIP, 5298);
       MemoReceived.Lines.Add('Added to roster: ' + PeerName);
-    end else
+    end
+    else
       MemoReceived.Lines.Add('Peer already in roster: ' + PeerName);
     if ListBoxContacts.Items.IndexOf(PeerName) = -1 then
     begin
       ListBoxContacts.Items.Add(PeerName);
       MemoReceived.Lines.Add('Added to ListBox: ' + PeerName);
-    end else
+    end
+    else
       MemoReceived.Lines.Add('Already in ListBox: ' + PeerName);
     Application.ProcessMessages;
-  end else
+  end
+  else
     MemoReceived.Lines.Add('Invalid peer data received!');
 end;
 
@@ -341,7 +276,8 @@ begin
     SendMessage(IdTCPClient1, SelectedContact.IP, SelectedContact.Port,
       'Hello, Pidgin!', 'inky@lovelace',
       SelectedContact.Username + '@' + SelectedContact.Hostname);
-  end else
+  end
+  else
   begin
     SendMessage(IdTCPClient1, SelectedContact.IP, SelectedContact.Port,
       MemoSend.Lines.Text, 'inky@lovelace',
@@ -359,10 +295,8 @@ procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
   if IdTCPServer1.Active then
     IdTCPServer1.Active := False;
-  if IdIPMCastServer1.Active then
-    IdIPMCastServer1.Active := False;
-  if Assigned(FReceiverThread) then
-    FReceiverThread.Terminate;
+  if IdUDPServer1.Active then
+    IdUDPServer1.Active := False;
   Roster.Free;
 end;
 
@@ -379,7 +313,8 @@ begin
   begin
     FLastReceivedMessage := AContext.Connection.IOHandler.AllData;
     TThread.Synchronize(nil, @DisplayReceivedMessage);
-  end else
+  end
+  else
   begin
     FLastReceivedMessage := 'Received empty or no data.';
     TThread.Synchronize(nil, @DisplayReceivedMessage);
